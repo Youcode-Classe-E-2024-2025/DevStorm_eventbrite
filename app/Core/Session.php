3a8084436cefb62460ac11f1e58c31093e3e6760
Tawdi <ahmedtawdi00@gmail.com>
2025-02-11 00:22:57 +0100

    ajoute des fonction pour message (session)

--- app/Core/Session.php
+++ app/Core/Session.php
@@ -21,4 +21,43 @@ class Session {
     }
 
 
+    public static function setFlashMessage($type, $message) {
+        $_SESSION[$type] = $message;
+    }
+
+    public static function hasFlashMessage($type) {
+        return isset($_SESSION[$type]);
+    }
+   
+    public static function getFlashMessage($type) {
+        if (self::hasFlashMessage($type)) {
+            $message = $_SESSION[$type];
+            unset($_SESSION[$type]);
+            return $message;
+        }
+        return null;
+    }
+
+    public static function setData($name,$content){
+        $_SESSION[$name] = $content;
+    }
+    
+    public static function getData($name){
+        if (isset($_SESSION[$name])) {
+            $content = $_SESSION[$name];
+            unset($_SESSION[$name]);
+            return $content;
+        }
+        return null;
+    }
+
+    public static function getUser(){
+        if (isset($_SESSION['user'])) {
+            $content = $_SESSION['user'];
+            return $content;
+        }
+        return null;
+    }
+
+
 }
