286f47eadac4759e8acb0773b746852fa56790c2
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-02-10 21:22:19 +0100

    create OrganizerController in back Controller

--- routes/routes.php
+++ routes/routes.php
@@ -4,12 +4,13 @@ use App\controllers\back\UserController;
 use App\controllers\back\DashboardController;
 use App\controllers\front\HomeController;
 
+
 use App\Core\Router;
 
 $router = new Router();
 
 
-$router->get('/', [HomeController::class, 'index']);
+$router->get('/ev', [HomeController::class, 'index']);
 $router->get('/registerForm', [UserController::class, 'register']);
 $router->get('/register', [UserController::class, 'register']);
 $router->post('/register', [UserController::class, 'handleRegister']);
@@ -17,5 +18,8 @@ $router->get('/login', [UserController::class, 'login']);
 $router->post('/login', [UserController::class, 'handleLogin']);
 $router->get('/logout', [UserController::class, 'logout']);
 $router->get('/dashboard', [DashboardController::class, 'Dashboard']);
+
+
+
 
 $router->dispatch();
