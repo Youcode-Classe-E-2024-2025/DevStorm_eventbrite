c586235c78cdd4b60f8a4a22c2c01d6b7f0ab105
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-02-10 22:15:48 +0100

    update pass database

--- routes/routes.php
+++ routes/routes.php
@@ -21,7 +21,12 @@ $router->post('/login', [UserController::class, 'handleLogin']);
 $router->get('/logout', [UserController::class, 'logout']);
 $router->get('/dashboard', [DashboardController::class, 'Dashboard']);
 
-
+//organizer ROUTEs
+$router->get('/organizer/event/create', [OrganizerController::class, 'createEvent']);
+$router->post('/organizer/event/create', [OrganizerController::class, 'handleCreateEvent']);
+$router->get('/organizer/event/stats/{id}', [OrganizerController::class, 'salesStats']);
+$router->get('/organizer/event/export/{id}', [OrganizerController::class, 'exportParticipants']);
+$router->get('/organizer/dashboard', [OrganizerController::class, 'dashboard']);
 
 
 
