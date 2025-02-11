9d025f4322db1afdb163c477b59ba18a8cf0967f
Tawdi <ahmedtawdi00@gmail.com>
2025-02-11 13:10:36 +0100

    ajoute de methode de toObject pour User model

--- app/models/User.php
+++ app/models/User.php
@@ -15,14 +15,17 @@ class User extends Model {
     public $username;
     public $email;
     public $password;
-    public  $role;
+    public $role;
+    public $avatar;
 
-    public function __construct($name=null,$email=null,$password=null,$role = Role::PARTICIPANT)
+    public function __construct($id=null,$name=null,$email=null,$password=null,$role = Role::PARTICIPANT,$avatar=null)
     {
+        $this->id=$id;
         $this->username = $name;
         $this->email = $email;
         $this->password = $password;
         $this->role = $role;
+        $this->avatar = $avatar;
     }
     public static function read($id)
     {
@@ -35,29 +38,22 @@ class User extends Model {
 
         $user=null;
         if($data){
-            $user = new User($data['name'],$data['email'],'',Role::from($data['role']));
-            $user->id = $data['id'];
+            $user = new User($data['id'],$data['name'],$data['email'],'',Role::from($data['role']),$data['avatar']);
         }
         return $user;
     } 
 
-    public static function findByEmail($email){
-        $db = Database::getInstance();
-        $query = $db->getConnection()->prepare("SELECT * FROM " . (new static())->getTable() . " WHERE email = :email");
-        $query->bindParam(':email', $email);
-        $query->execute();
-        return $query->fetchObject('App\Models\User');
-    }
-
     public function save()
     {
         $db = Database::getInstance();
-        $query = $db->getConnection()->prepare("INSERT INTO " . $this->getTable() . " (name, email, password_hash, role) VALUES (:username, :email, :password, :role)");
+        $query = $db->getConnection()->prepare("INSERT INTO " . $this->getTable() . " (name, email, password_hash, role, avatar) VALUES (:username, :email, :password, :role, :avatar)");
         $query->bindParam(':username', $this->username);
         $query->bindParam(':email', $this->email);
         $query->bindParam(':password', $this->password);
         $role_name = $this->role->value;
+        $avatar = $this->avatar ?? 'empty';
         $query->bindParam(':role', $role_name);
+        $query->bindParam(':avatar', $avatar);
         if ($query->execute()) {
             return $db->getConnection()->lastInsertId();
         } else {
@@ -81,11 +77,26 @@ class User extends Model {
 
     }
 
+    /**
+     * create  objects 
+     * 
+     * @param array $rows
+     * @return array
+     */
+    private static function toObjects($rows){
+        $users=[];
+        foreach ($rows as $row) {
+             $users[] =  new User($row['id'],$row['name'],$row['email'],'',Role::from($row['role']),$row['avatar']);
+         }
+         return $users;
+       }
+
     public function fetchAll(){
         $db = Database::getInstance();
         $query = $db->getConnection()->prepare("SELECT * FROM " . $this->getTable());
         $query->execute();
-        return $query->fetchAll();
+        $rows = $query->fetchAll();
+        return self::toObjects($rows);
     }
     
 }
