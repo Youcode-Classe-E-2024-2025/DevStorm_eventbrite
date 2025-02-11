317238d342ed6f922e08e91c2002208799927825
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-02-11 14:39:44 +0100

    add the admin dashboard first display the vents and the theirs status and actions like edit and delete

--- app/controllers/back/DashboardController.php
+++ app/controllers/back/DashboardController.php
@@ -3,8 +3,16 @@
 namespace App\controllers\back;
 use App\Core\Controller;
 use App\Core\Session;
+use App\Models\Event;
 use App\Models\User;
 use App\models\Article;
 
 class DashboardController extends Controller{
+
+    public function adminDashboard(){
+        $event = new Event();
+        $events = $event->getAllEvents();
+        $this->view('front/adminDashboard',['events'=>$events]);
+    }
+
 }
